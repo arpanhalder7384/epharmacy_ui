efaf41288f61571f49cee84f84e369595592066d
Arpan Halder <arpanhalder7384@gmail.com>
2025-02-24 11:59:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div class="flex-row bg-red-500">
+    <div>E Pharmacy</div>
+    <div>H22</div>
+    </div>
+  );
+}
+
+export default App;
